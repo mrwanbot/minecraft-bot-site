c599efe85ee799bc0da2e54b7747ca28529e89e3
mrwanbot <mrwanada2024@gmail.com>
2026-06-18 12:18:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function startBot(){
-alert("تم الضغط على تشغيل");
+let statusText = document.getElementById("status");
+
+function startBot() {
+  statusText.innerHTML = "Online 🟢";
+  statusText.style.color = "lime";
+}
+
+function stopBot() {
+  statusText.innerHTML = "Offline 🔴";
+  statusText.style.color = "red";
+}
+
+function sendCommand() {
+  let cmd = document.getElementById("command").value;
+  if(cmd === "") {
+    alert("اكتب أمر أولاً!");
+  } else {
+    alert("Command sent: " + cmd);
+    document.getElementById("command").value = "";
+  }
 }
